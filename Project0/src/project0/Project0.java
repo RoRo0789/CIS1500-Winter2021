34125c2f96943403a3e34fd0097ef5e5620ca5cb
Eric Charnesky <eric.charnesky@careevolution.com>
2021-01-13 11:46:20 -0500

    Project0

---
+++ Project0/src/project0/Project0.java
@@ -0,0 +1,13 @@
+package project0;
+
+public class Project0 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello computer, this is Eric!");
+        System.out.println("Hello Eric, how are you today?");
+        System.out.println("I'm having a bad day, I'm dumb and showed me password");
+    }
+    
+}
