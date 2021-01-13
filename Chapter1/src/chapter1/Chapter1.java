1b5d69fe6abc875c35ee253f04085be3036b2e43
Eric Charnesky <eric.charnesky@careevolution.com>
2021-01-13 11:25:03 -0500

    changed comments

--- Chapter1/src/chapter1/Chapter1.java
+++ Chapter1/src/chapter1/Chapter1.java
@@ -1,19 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chapter1;
 
-/**
- *
- * @author EricC
- */
 public class Chapter1 {
 
-    /**
-     * @param args the command line arguments
-     */
+    // this is a comment and is ignored by the java compiler
     public static void main(String[] args) {
         double totalCreditsRequired = 62;
         double creditsCompleted = 32;
@@ -21,7 +10,6 @@ public class Chapter1 {
         double creditsPerSemester = 12;
         double semestersRemaining = Math.ceil(creditsRemaining / creditsPerSemester);
         System.out.println("You have " + semestersRemaining + " semesters left!");
-
     }
 
 }
