edefdee1aa0e50466502d84b16148f9e1b9d5f7f
Eric Charnesky <eric.charnesky@careevolution.com>
2021-01-20 11:21:48 -0500

    more type examples

--- Chapter2-part1/src/chapter2/part1/Chapter2Part1.java
+++ Chapter2-part1/src/chapter2/part1/Chapter2Part1.java
@@ -1,21 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chapter2.part1;
 
-/**
- *
- * @author EricC
- */
+import java.util.Scanner;
+
 public class Chapter2Part1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // this is a comment line
+        System.out.println("Hello World!");
+        System.out.println("Another line!");
+        // \n is the newline character
+        System.out.print("Hi, I'm a print command\n");
+        System.out.print("\tI'm another print command!\n");
+        System.out.println("\"Happy Wednesday\", said Eric");
+        
+        // delcaring the variable
+        String myFirstName;
+        
+        // assigning a value to the variable
+        myFirstName = "Eric";
+        
+        // or all at once
+        String myLastName = "Charnesky";
+        
+        int favoriteNumber = 42;
+        double numberWithDecimal = 1.23;
+        
+        
+        System.out.println("Hi there " + myFirstName + " " + myLastName);
+        System.out.println("if we have some really long line of text and "
+                + " heres some more text on another line" + (6 + 7)
+                + " remember to start a new quote ( string ) on every line");
+        
+        
+        Scanner keyboard = new Scanner(System.in);
     }
     
 }
