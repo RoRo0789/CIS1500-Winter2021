f241077871f66c32744eed419bb58690ff7b090c
Eric Charnesky <eric.charnesky@careevolution.com>
2021-02-08 10:51:17 -0500

    Project 1 complete

--- Project1/src/project1/Project1.java
+++ Project1/src/project1/Project1.java
@@ -1,21 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project1;
 
-/**
- *
- * @author EricC
- */
+import java.util.Scanner;
+
 public class Project1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String name;
+        int distanceBetweenPosts;
+        int totalLength;
+        int totalWidth;
+        int perimeter;
+        int boardLength;
+        int usableBoardLength;
+        int totalScrapBoard;
+        int numberOfBoardsToPurchase;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("What's your name contractor friend?");
+        name = keyboard.nextLine();
+
+        System.out.println(name + ", how far apart are your posts going to be?");
+        distanceBetweenPosts = Integer.parseInt(keyboard.nextLine());
+
+        System.out.println(name + ", what is the total length?");
+        totalLength = Integer.parseInt(keyboard.nextLine());
+
+        System.out.println(name + ", what is the total width?");
+        totalWidth = Integer.parseInt(keyboard.nextLine());
+
+        System.out.println(name + ", what length board are you going to buy?");
+        boardLength = Integer.parseInt(keyboard.nextLine());
+
+        perimeter = totalLength * 2 + totalWidth * 2;
+
+        // we're expecting integer division to truncate any decimal
+        usableBoardLength = (boardLength / distanceBetweenPosts) * distanceBetweenPosts;
+
+        // expecting integer division again
+        numberOfBoardsToPurchase = perimeter / usableBoardLength;
+        
+        // if you used a double and a rounding function
+        double easierCalculationNumberOfBoardToPurchase = Math.ceil((double)perimeter / usableBoardLength);
+        
+
+        // remiainder or modulus operator
+        if (perimeter % usableBoardLength > 0) {
+            numberOfBoardsToPurchase += 1;
+        }
+        
+        totalScrapBoard = (numberOfBoardsToPurchase * boardLength) - perimeter;
+
+        System.out.printf("%s you need to purchase %d boards\n", name, numberOfBoardsToPurchase);
+        System.out.printf("You will have %d feet of scrap board left over\n", totalScrapBoard);
     }
-    
+
 }
