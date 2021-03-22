a04291b3ca6413fac895dbe0aa67541b54f6cfbe
Eric Charnesky <eric.charnesky@careevolution.com>
2021-03-22 10:46:44 -0400

    Project 3 complete

--- Project3/src/project3/Project3.java
+++ Project3/src/project3/Project3.java
@@ -1,8 +1,19 @@
 package project3;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Project3 {
 
-    public static void main(String[] args) {
+    static Scanner keyboard;
+    static String gameIntroductionFile = "gameIntroduction.txt";
+    static String escapeWordFileName = "escapeWord.txt";
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        keyboard = new Scanner(System.in);
 
         String mode = askForPlayOrCreateMode();
 
@@ -14,41 +25,125 @@ public class Project3 {
     }
 
     private static String askForPlayOrCreateMode() {
-        return "";
+
+        String choice = "";
+
+        while (!choice.equalsIgnoreCase("play") && !choice.equalsIgnoreCase("create")) {
+            System.out.println("Do you want to Create or Play the game?");
+            choice = keyboard.nextLine();
+        }
+
+        return choice;
     }
 
-    private static void playMode() {
-        // you do this part
+    private static void playMode() throws FileNotFoundException {
+        readGameIntroduction();
+        String escapeWord = getEscapeWord();
+        String command = "";
+
+        while (!command.equalsIgnoreCase(escapeWord)) {
+            System.out.println("What do you want to do?");
+            command = keyboard.nextLine();
+
+            if (!command.equalsIgnoreCase(escapeWord)) {
+                tryCommand(command + ".txt");
+            }
+        }
+        System.out.println("Thanks for playing our terrible game!");
     }
 
-    private static void createMode() {
+    private static void createMode() throws FileNotFoundException {
         createGameIntroductionFile();
         createCommandFiles();
         createEscapeWordFile();
     }
 
-    private static void createGameIntroductionFile() {
-        // ask the user to enter the game introduction, and write it to a file named GameIntroduction.txt
+    private static void createGameIntroductionFile() throws FileNotFoundException {
+        System.out.println("Please enter the game introduction: ");
+        String intro = keyboard.nextLine();
+        PrintWriter fileWriter = new PrintWriter(gameIntroductionFile);
+        fileWriter.println(intro);
+        fileWriter.close();
     }
 
-    private static void createCommandFiles() {
+    private static void createCommandFiles() throws FileNotFoundException {
         String fileName = "";
 
-        while (!fileName.equalsIgnoreCase("SOME ESCAPE WORD")) {
-            // needs some work - don't create file with the sentinel value name,
-            // also ask for filename...
-            createCommandFile(fileName);
+        while (!fileName.equalsIgnoreCase("STOP CREATE MODE")) {
+            System.out.println("Enter the name of a command for the game or "
+                    + "'STOP CREATE MODE' to be done entering commands");
+            fileName = keyboard.nextLine();
+            if (!fileName.equalsIgnoreCase("STOP CREATE MODE")) {
+                createCommandFile(fileName + ".txt");
+            }
         }
     }
 
-    private static void createCommandFile(String fileName) {
-        // ask if this command will wound the player, if it does, write WOUNDED to the first line of the file, otherwise write a blank line
+    private static void createCommandFile(String fileName) throws FileNotFoundException {
+        System.out.println("Will this command wound the player? Y/N");
 
-        // then ask for what happens when the user enters that command, write that to the file
+        String woundsPlayer = keyboard.nextLine();
+
+        PrintWriter fileWriter = new PrintWriter(fileName);
+
+        if (woundsPlayer.equalsIgnoreCase("Y")) {
+            fileWriter.println("WOUNDED");
+        } else {
+            fileWriter.println("");
+        }
+        System.out.println("Describe what happens when the player does this command?");
+        String description = keyboard.nextLine();
+        fileWriter.println(description);
+        fileWriter.close();
     }
 
-    private static void createEscapeWordFile() {
-        // ask for a secret escape word/phrase that wins the game - write that to escapeWord.txt
+    private static void createEscapeWordFile() throws FileNotFoundException {
+        System.out.println("Enter a secret escape word that ends the game");
+        String escapeWord = keyboard.nextLine();
+
+        PrintWriter fileWriter = new PrintWriter(escapeWordFileName);
+        fileWriter.println(escapeWord);
+        fileWriter.close();
     }
 
+    private static void readGameIntroduction() throws FileNotFoundException {
+        File intro = new File(gameIntroductionFile);
+        Scanner fileReader = new Scanner(intro);
+        while (fileReader.hasNext()) {
+            System.out.println(fileReader.nextLine());
+        }
+        fileReader.close();
+    }
+
+    private static String getEscapeWord() throws FileNotFoundException {
+        File escapeWordFile = new File(escapeWordFileName);
+        Scanner fileReader = new Scanner(escapeWordFile);
+        String escapeWord = fileReader.nextLine();
+        fileReader.close();
+        return escapeWord;
+    }
+
+    private static void tryCommand(String command) throws FileNotFoundException {
+        File commandFile = new File(command);
+        if (commandFile.exists()) {
+            Scanner fileReader = new Scanner(commandFile);
+            String woundsPlayerLine = fileReader.nextLine();
+            String description = fileReader.nextLine();
+
+            System.out.println(description);
+
+            if (woundsPlayerLine.equalsIgnoreCase("WOUNDED")) {
+                String sleepCommand = "";
+
+                while (!sleepCommand.equalsIgnoreCase("sleep")) {
+
+                    System.out.println("You are wounded and must sleep");
+                    sleepCommand = keyboard.nextLine();
+                }
+                System.out.println("You sleep and are miraculously healed!");
+            }
+        } else {
+            System.out.println("You can't do that!");
+        }
+    }
 }
