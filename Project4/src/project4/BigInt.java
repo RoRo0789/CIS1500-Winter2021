31209bfd7732af6c17b43baad312593797057157
Eric Charnesky <eric.charnesky@careevolution.com>
2021-04-07 11:31:07 -0400

    BigInt

--- Project4/src/project4/BigInt.java
+++ Project4/src/project4/BigInt.java
@@ -1,54 +1,217 @@
 package project4;
 
+import java.util.ArrayList;
+
 public class BigInt {
 
-    public BigInt()
-    {
-        // makes empty big Int
+    private ArrayList<Integer> digits;
+    private boolean positive;
+
+    // only for us to use in the add/substract methods
+    private BigInt() {
+        digits = new ArrayList<Integer>();
+        positive = true;
     }
-    
+
     public BigInt(String value) {
-        // fixme!
+        positive = true;
+        digits = new ArrayList<Integer>();
+        int index = 0;
+        if (value.startsWith("-")) {
+            positive = false;
+            index = 1;
+        }
+        while (index < value.length()) {
+            if (value.charAt(index) != ',') {
+                digits.add(Integer.parseInt(Character.toString(value.charAt(index))));
+            }
+            index++;
+        }
     }
 
     private boolean isBigger(BigInt other) {
-        return true; // fixme
+        if (digits.size() > other.digits.size()) {
+            return true;
+        }
+        if (digits.size() < other.digits.size()) {
+            return false;
+        }
+        for (int index = 0; index < digits.size(); index++) {
+            if (digits.get(index) > other.digits.get(index)) {
+                return true;
+            }
+            if (digits.get(index) < other.digits.get(index)) {
+                return false;
+            }
+        }
+        return false;
     }
 
     public BigInt add(BigInt other) {
-        // do something
+        if (positive && other.positive || !positive && !other.positive) {
+            if (isBigger(other)) {
+                BigInt result = addSmallerToBiggerIgnoreSign(other);
+                result.positive = positive;
+                return result;
+            } else {
+                BigInt result = other.addSmallerToBiggerIgnoreSign(this);
+                result.positive = other.positive;
+                return result;
+            }
+        }
+
+        if (isBigger(other)) {
+            BigInt result = subtractSmallerFromBiggerIgnoreSign(other);
+            result.positive = positive;
+            return result;
+        } else {
+            BigInt result = other.subtractSmallerFromBiggerIgnoreSign(this);
+            result.positive = other.positive;
+            return result;
+        }
+
+    }
+
+    private BigInt addSmallerToBiggerIgnoreSign(BigInt other) {
         BigInt newResult = new BigInt();
-        // do not change yourself or the other, return a new big int
+
+        int myIndex = digits.size() - 1;
+        int otherIndex = other.digits.size() - 1;
+
+        boolean carry = false;
+        while (myIndex >= 0 && otherIndex >= 0) {
+            int newDigit = digits.get(myIndex) + other.digits.get(otherIndex);
+
+            if (carry) {
+                newDigit++;
+                carry = false;
+            }
+
+            if (newDigit >= 10) {
+                carry = true;
+                newDigit -= 10;
+            }
+            newResult.digits.add(0, newDigit);
+
+            myIndex--;
+            otherIndex--;
+        }
+
+        while (myIndex >= 0) {
+            int newDigit = digits.get(myIndex);
+            if (carry) {
+                newDigit++;
+                carry = false;
+            }
+
+            if (newDigit >= 10) {
+                carry = true;
+                newDigit -= 10;
+            }
+            newResult.digits.add(0, newDigit);
+            myIndex--;
+
+        }
         
-        // do stuff here to add digits to newResult
-        
-        
-        // 123
-        // -45
-        // ---
-        //  78
-        
+        if ( carry ){
+            newResult.digits.add(0, 1);
+        }
+
+        newResult.positive = positive;
+
         return newResult;
     }
 
-    public BigInt subtract(BigInt other) {
-        // do something
+    private BigInt subtractSmallerFromBiggerIgnoreSign(BigInt other) {
+        BigInt result = new BigInt();
 
-        // do not change yourself or the other, return a new big int
-        if (isBigger(other)) {
+        int myIndex = digits.size() - 1;
+        int otherIndex = other.digits.size() - 1;
 
-        } else {
+        boolean borrowed = false;
+        while (myIndex >= 0 && otherIndex >= 0) {
+            int myDigit = digits.get(myIndex);
+
+            if (borrowed) {
+                myDigit--;
+                borrowed = false;
+            }
+
+            int otherDigit = other.digits.get(otherIndex);
+
+            if (otherDigit > myDigit) {
+                myDigit += 10;
+                borrowed = true;
+            }
+
+            result.digits.add(0, myDigit - otherDigit);
+
+            myIndex--;
+            otherIndex--;
 
         }
-        return new BigInt();
+
+        while (myIndex >= 0) {
+            int myDigit = digits.get(myIndex);
+
+            if (borrowed) {
+                myDigit--;
+                borrowed = false;
+            }
+            result.digits.add(0, myDigit);
+
+            myIndex--;
+
+        }
+
+        return result;
+
+    }
+
+    public BigInt subtract(BigInt other) {
+        // do not change yourself or the other, return a new big int
+        if (positive && other.positive || !positive && !other.positive) {
+            if (isBigger(other)) {
+                BigInt result = subtractSmallerFromBiggerIgnoreSign(other);
+                result.positive = positive;
+                return result;
+            } else {
+                BigInt result = other.subtractSmallerFromBiggerIgnoreSign(this);
+                result.positive = !other.positive;
+                return result;
+            }
+        }
+        
+        if ( isBigger(other)){
+            BigInt result = addSmallerToBiggerIgnoreSign(other);
+            result.positive = positive;
+            return result;
+        }
+        else{
+            BigInt result = other.addSmallerToBiggerIgnoreSign(this);
+            result.positive = positive;
+            return result;
+        }
+
+
     }
 
     public String toString() {
         // return a nicely formatted string with thousands seperator
         // ie -12,345,678
-        
+
         StringBuilder stringBuilder = new StringBuilder();
-        
-        return "";
+
+        if (!positive) {
+            stringBuilder.append("-");
+        }
+
+        for (int index = 0; index < digits.size(); index++) {
+            if (index != 0 && (digits.size() - index) % 3 == 0) {
+                stringBuilder.append(",");
+            }
+            stringBuilder.append(digits.get(index));
+        }
+        return stringBuilder.toString();
     }
 }
--- Project4/test/project4/BigIntTest.java
+++ Project4/test/project4/BigIntTest.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package project4;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author EricC
- */
+
 public class BigIntTest {
     
     public BigIntTest() {
@@ -29,11 +22,151 @@ public class BigIntTest {
     }
     
     @Test
+    public void testAddTwoNegatives() {
+        BigInt first = new BigInt("-123");
+        BigInt second = new BigInt("-234");
+        
+        BigInt result = first.add(second);
+        String actualValue = result.toString();
+        
+        assertEquals("-357", actualValue);
+    }
+    
+    @Test
     public void testAddPositiveAndNegative() {
+        BigInt first = new BigInt("-123");
+        BigInt second = new BigInt("234");
+        
+        BigInt result = first.add(second);
+        String actualValue = result.toString();
+        
+        assertEquals("111", actualValue);
+    }
+    
+    @Test
+    public void testAddPositiveAndNegativeAgain() {
+        BigInt first = new BigInt("-123");
+        BigInt second = new BigInt("234");
+        
+        BigInt result = second.add(first);
+        String actualValue = result.toString();
+        
+        assertEquals("111", actualValue);
+    }
+    
+    @Test
+    public void testAddPositiveAndNegativeWithNegativeResult() {
+        BigInt first = new BigInt("123");
+        BigInt second = new BigInt("-234");
+        
+        BigInt result = first.add(second);
+        String actualValue = result.toString();
+        
+        assertEquals("-111", actualValue);
+    }
+    
+    @Test
+    public void testAddPositiveAndNegativeAgainWithNegativeResult() {
+        BigInt first = new BigInt("123");
+        BigInt second = new BigInt("-234");
+        
+        BigInt result = second.add(first);
+        String actualValue = result.toString();
+        
+        assertEquals("-111", actualValue);
     }
     
     @Test
     public void testAddIntsOfDifferentSizes() {
+        BigInt first = new BigInt("999999");
+        BigInt second = new BigInt("1");
+        
+        BigInt result = second.add(first);
+        String actualValue = result.toString();
+        
+        assertEquals("1,000,000", actualValue);
+        assertEquals("999,999", first.toString());
+        assertEquals("1", second.toString());
+        
+    }
+    
+    @Test
+    public void testToStringNegative()
+    {
+        // Arrange
+        String expectedOutput = "-12,345,678";
+        
+        // Act
+        BigInt bigInt = new BigInt(expectedOutput);
+        String actualOutput = bigInt.toString();
+        
+        // Assert
+        assertEquals(expectedOutput, actualOutput);
+    }
+    
+    @Test
+    public void testToStringPositiveWithoutCommas()
+    {
+        // Arrange
+        String expectedOutput = "678";
+        
+        // Act
+        BigInt bigInt = new BigInt(expectedOutput);
+        String actualOutput = bigInt.toString();
+        
+        // Assert
+        assertEquals(expectedOutput, actualOutput);
+    }
+    
+    
+    @Test
+    public void testSubtractionBothPositive()
+    {
+        BigInt first = new BigInt("999999");
+        BigInt second = new BigInt("1");
+        
+        BigInt result = first.subtract(second);
+        String actualValue = result.toString();
+        
+        assertEquals("999,998", actualValue);
+    }
+    
+    @Test
+    public void testSubtractionBothNegative()
+    {
+        BigInt first = new BigInt("-999999");
+        BigInt second = new BigInt("-1");
+        
+        BigInt result = first.subtract(second);
+        String actualValue = result.toString();
+        
+        assertEquals("-999,998", actualValue);
+    }
+    
+    @Test
+    public void testSubtractionNegativeMinusPositive()
+    {
+        BigInt first = new BigInt("-999999");
+        BigInt second = new BigInt("1");
+        
+        BigInt result = first.subtract(second);
+        String actualValue = result.toString();
+        
+        assertEquals("-1,000,000", actualValue);
+        assertEquals("-999,999", first.toString());
+        assertEquals("1", second.toString());
+    }
+    
+    @Test
+    public void testSubtractionPosiitveMinusNegative()
+    {
+        BigInt first = new BigInt("999999");
+        BigInt second = new BigInt("-1");
+        
+        BigInt result = first.subtract(second);
+        String actualValue = result.toString();
+        
+        assertEquals("1,000,000", actualValue);
     }
     
 }
