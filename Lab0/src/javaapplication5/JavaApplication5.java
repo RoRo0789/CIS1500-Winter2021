69fd16b8ac994135dea279c89678462f3ef51724
RoRo0789 <97565647+RoRo0789@users.noreply.github.com>
2022-01-13 11:25:53 -0500

    Lab 0

---
+++ Lab0/src/javaapplication5/JavaApplication5.java
@@ -0,0 +1,17 @@
+
+package javaapplication5;
+
+public class JavaApplication5 {
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Rochelle Duval");
+        System.out.println("prounoun she/her");
+        System.out.println("Computer Information Systems Software Engineering");
+        System.out.println("Intro to Java taughy by Asmi Mehta");
+        System.out.println("The class is required for my degree");
+        System.out.println("I am a fraternal twin, a hairdresser, and a pantry cook!");
+        System.out.println("I am looking forward to our semester, thank you!");
+    }
+    
+}
